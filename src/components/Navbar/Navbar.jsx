ced43b73428507efa54ca53e1161319ffd946438
Iuri Torres <iuri.t1000@gmail.com>
2023-05-24 17:17:17 -0300

    footer finished

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,4 +1,5 @@
 import React, { useState } from 'react'
+import { Link } from 'react-router-dom'
 import OverlayTrigger from 'react-bootstrap/OverlayTrigger'
 import Popover from 'react-bootstrap/Popover'
 
@@ -144,10 +145,10 @@ const Navbar = () => {
     return (
         <div className="main-nav-bar bg-white w-100">
             <div className="container py-3 d-flex justify-content-between align-items-center">
-                <a href="/" className="d-flex align-items-center gap-2 text-decoration-none">
+                <Link to="/" className="d-flex align-items-center gap-2 text-decoration-none">
                     <img src="/logo-svg.svg" className="logo" alt="bytecode - coding the future logo" />
                     <h2 className="fs-1">byte<span className="purple-span">code</span></h2>
-                </a>
+                </Link>
 
                 <ul className="w-50 list-unstyled m-0 d-flex gap-5 align-items-center">                    
                     <OverlayTrigger
