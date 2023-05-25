25d576b96779c9ac1630ae1bb44b04c3b36d62e5
Iuri Torres <iuri.t1000@gmail.com>
2023-05-25 12:42:20 -0300

    adjusts

--- src/App.jsx
+++ src/App.jsx
@@ -1,7 +1,10 @@
-import Home from "./pages/Home/Home"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
+import Home from "./pages/Home/Home"
+
 const App = () => {
+
+    // ROUTES
     return (
         <BrowserRouter>
             <Routes>
