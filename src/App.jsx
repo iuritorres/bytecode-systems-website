6d1e60f8603bc51f8e595b2e3d155d2f7dfabf94
Iuri Torres <iuri.t1000@gmail.com>
2023-05-29 12:54:41 -0300

    added vlibras accessibility

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from "react"
 import { BrowserRouter, Routes, Route } from "react-router-dom"
 
 import Home from "./pages/Home/Home"
@@ -7,6 +8,15 @@ const App = () => {
     // ROUTES
     return (
         <BrowserRouter>
+            {/* VLIBRAS */}
+            <div vw="true" className="enabled">
+                <div vw-access-button="true" className="active"></div>
+                    <div vw-plugin-wrapper="true">
+                    <div className="vw-plugin-top-wrapper"></div>
+                </div>
+            </div>
+
+            {/* ROUTES */}
             <Routes>
                 <Route exact path="/" element={<Home/>} />
             </Routes>
