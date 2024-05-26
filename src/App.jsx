84453290a798be4b817f0fcf1714ce4adb5a8695
iuritorres <iuri.t1000@gmail.com>
2024-05-25 22:22:51 -0300

    refactor: change home path from '/website/' to '/'

--- src/App.jsx
+++ src/App.jsx
@@ -1,26 +1,25 @@
-import { BrowserRouter, Routes, Route } from "react-router-dom"
+import { BrowserRouter, Route, Routes } from "react-router-dom"
 
 import Home from "./pages/Home/Home"
 
 const App = () => {
+  // ROUTES
+  return (
+    <BrowserRouter>
+      {/* V LIBRAS */}
+      <div vw='true' className='enabled'>
+        <div vw-access-button='true' className='active'></div>
+        <div vw-plugin-wrapper='true'>
+          <div className='vw-plugin-top-wrapper'></div>
+        </div>
+      </div>
 
-    // ROUTES
-    return (
-        <BrowserRouter>
-            {/* V LIBRAS */}
-            <div vw="true" className="enabled">
-                <div vw-access-button="true" className="active"></div>
-                    <div vw-plugin-wrapper="true">
-                    <div className="vw-plugin-top-wrapper"></div>
-                </div>
-            </div>
+      {/* ROUTES */}
+      <Routes>
+        <Route exact path='/' element={<Home />} />
+      </Routes>
+    </BrowserRouter>
+  );
+};
 
-            {/* ROUTES */}
-            <Routes>
-                <Route exact path="/website/" element={<Home/>} />
-            </Routes>
-        </BrowserRouter>
-    )
-}
-
-export default App
+export default App;
